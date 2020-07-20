f4e688b52a077b34a455e0333b56048f708f2cab
lee-coates <lee.coates.hackathons@gmail.com>
2020-07-19 18:44:34 -0700

    scaffolded project for makefiles

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,17 +1,2 @@
-import pandas as pd
-import json
-import requests
 
-#Import Ridership Data from the API into a DataFrame
-ridership_api_data = requests.get('http://service.civicpdx.org/transportation-systems/passenger-census/passenger-census/').json()
-raw_ridership_data = pd.DataFrame(ridership_api_data)
-l = raw_ridership_data['results']['features']
-
-#Import Ridership Data from the API into a DataFrame
-ridership_api_data = requests.get('http://service.civicpdx.org/transportation-systems/passenger-census/system/annual/total/').json()
-raw_ridership_data = pd.io.json.json_normalize(ridership_api_data)
-
-#Import National Data
-national_ridership_api_data = requests.get('http://service.civicpdx.org/transportation-systems/passenger-census/national/ridership/').json()
-national_ridership_data = pd.io.json.json_normalize(national_ridership_api_data)
 
